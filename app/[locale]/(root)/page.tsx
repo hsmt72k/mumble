b19a2f7ef3e4767a2f13da63fbb6692325e280f8
hsmt72k <hsmt72k@gmail.com>
2024-01-13 17:04:04 +0900

    add locale to clerk webhooks

--- app/[locale]/(root)/page.tsx
+++ app/[locale]/(root)/page.tsx
@@ -1,17 +1,17 @@
 import { currentUser } from '@clerk/nextjs';
 
-import { fetchPosts } from '@/lib/actions/post.actions';
-import PostCard from '@/components/cards/PostCard';
+// import { fetchPosts } from '@/lib/actions/post.actions';
+// import PostCard from '@/components/cards/PostCard';
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
-  const user = await currentUser();
+  // const result = await fetchPosts(1, 30);
+  // const user = await currentUser();
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
 
-      <section className="mt-9 flex flex-col gap-10">
+      {/* <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No posts found</p>
         ) : (
@@ -33,7 +33,7 @@ export default async function Home() {
             })}
           </>
         )}
-      </section>
+      </section> */}
     </>
   );
 }
